156f84535f95a172348cd74cf2547837ea22fccb
Muammer <muammerakca10@gmail.com>
2023-01-24 11:32:46 +0300

    Tried sound for A key

--- Xsylophone/ViewController.swift
+++ Xsylophone/ViewController.swift
@@ -6,16 +6,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
     @IBAction func aKeyTapped(_ sender: UIButton) {
+        playSound()
+        print("Tapped A")
     }
     
     @IBAction func bKeyTapped(_ sender: Any) {
@@ -34,6 +39,58 @@ class ViewController: UIViewController {
     }
     
     @IBAction func gKeyTapped(_ sender: Any) {
+    }
+    
+//    func playSound() {
+//        print("A will play")
+//
+//        guard let path = Bundle.main.path(forResource: "A", ofType:"wav") else { return }
+//
+//        let url = URL(fileURLWithPath: path)
+//
+//        do {
+//
+//            player = try AVAudioPlayer(contentsOf: url)
+//            print("player assigned")
+//
+//            player?.play()
+//            print("A played")
+//
+//        } catch let error {
+//
+//            print(error.localizedDescription)
+//
+//        }
+//
+//    }
+//
+//}
+
+    func playSound(){
+        
+        print("A will play")
+        
+        guard let url = Bundle.main.url(forResource: "Sounds/A", withExtension: "wav") else {return}
+        
+        print("url assign ok")
+
+        do{
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            
+            print("Set category ok")
+            
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            guard let player = player else {return}
+
+            player.play()
+            print("A played")
+
+        }catch let error {
+            print(error.localizedDescription)
+            print("Error")
+        }
     }
 }
 
